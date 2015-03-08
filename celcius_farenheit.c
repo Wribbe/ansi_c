60d83873c9fbea3a207655402db49879873e24a2
Stefan Eng <eng_steff@hotmail.com>
2015-03-08 21:45:02 +0100

    Use float to get better precision.

--- celcius_farenheit.c
+++ celcius_farenheit.c
@@ -4,7 +4,7 @@
    for fahr = 0, 20, ..., 300 */
 int main() 
 {
-        int fahrenheit, celsius;
+        float fahrenheit, celsius;
         int lower, upper, step;
 
         lower = 0;
@@ -13,8 +13,8 @@ int main()
 
         fahrenheit = lower;
         while (fahrenheit <= upper) {
-                celsius = 5 * (fahrenheit-32) / 9;
-                printf("%3d\t%6d\n", fahrenheit, celsius);
+                celsius = (5.0/9.0) * (fahrenheit-32);
+                printf("%3.0f\t%6.1f\n", fahrenheit, celsius);
                 fahrenheit = fahrenheit + step;
         }
 }
