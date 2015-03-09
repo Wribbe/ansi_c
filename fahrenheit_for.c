5039da892e02cdff9b8ba667ebc5ff9cd96b7d5d
Stefan Eng <eng_steff@hotmail.com>
2015-03-09 09:30:35 +0100

    Reverse order of printout.

--- fahrenheit_for.c
+++ fahrenheit_for.c
@@ -5,7 +5,7 @@ int main()
 {
         int fahrenheit;
 
-        for (fahrenheit=0; fahrenheit <= 300; fahrenheit = fahrenheit + 20) {
+        for (fahrenheit=300; fahrenheit >= 0; fahrenheit = fahrenheit - 20) {
                 printf("%3d %6.1f\n", fahrenheit, (5.0/9.0)*(fahrenheit-32));
         }
 }
