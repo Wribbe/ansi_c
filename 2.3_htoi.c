ccd36ac78ea680e8982e68b4f768e599b4b39701
Stefan Eng <eng_steff@hotmail.com>
2015-05-01 17:51:02 +0200

    Convert hex -> integer with optional 0x / 0X prefix.

--- 2.3_htoi.c
+++ 2.3_htoi.c
@@ -5,18 +5,46 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int htoi(char* string)
+void htoi(char* string)
 {
     int i;
-    int c;
+    int c, sum = 0;
     for(i = 0; (c = string[i]) != '\0'; i++) {
         c = tolower(c);
-        putchar(c);
+        if (c >= 'a' && c <= 'f') {
+            sum = sum*16 + c-'a'+10;
+        } else if (c >= '1' && c <= '9') {
+            sum = sum*16 + c-'0';
+        } else {
+            printf("skipping %c, not valid\n", c);
+            continue;
+        }
     }
-    return 1;
+    printf("result of %s is: %d\n",string, sum);
 }
 
-int main(void)
+int main(int argc, char* argv[])
 {
-    htoi("tesT");
+    if (argc < 2) {
+        printf("Need at least one parameter.\n");
+        exit(0);
+    }
+    char* string = argv[1];
+    int size = sizeof(string)/sizeof(string[0]);
+    if (size < 3) {
+        htoi(string);
+    } else {
+        int first, second, i;
+        first = tolower(string[0]);
+        second = tolower(string[1]);
+        if (first == '0' && second == 'x') {
+            char new_string[size-2];
+            for(i = 2; i<size; i++) {
+                new_string[i-2] = string[i];
+            }
+            htoi(new_string);
+        } else {
+            htoi(string);
+        }
+    }
 }
