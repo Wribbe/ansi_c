6aaa48f757d80786391512456b8f63f3edc34e69
Stefan Eng <eng_steff@hotmail.com>
2015-05-02 13:12:44 +0200

    Mark used chars in array.

---
+++ 2.4_squeeze.c
@@ -0,0 +1,30 @@
+/* 2.4 squeeze: write a functions that takes string1 and string2 and
+ * deletes all characters from string1 that matches any character in
+ * string2 */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+void trim(char* string1, char* string2)
+{
+    int chars[25] = { 0 };
+    int i,c = 0;
+    while ((c = string2[i++]) != '\0') {
+        printf("char: %c\n",c);
+        c = tolower(c)-'a';
+        chars[c] = 1;
+    }
+    for (int i = 0; i < 25; i++) {
+        printf("chars pos %d is %i.\n",i,chars[i]);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3) {
+        printf("Note enough parameters, provide at least 2.\n");
+        exit(1);
+    }
+    trim(argv[1],argv[2]);
+}
