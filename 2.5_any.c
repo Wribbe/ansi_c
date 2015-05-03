8bf0418e1a5370a89e4c77bbe831cda0e7ec03ce
Stefan Eng <eng_steff@hotmail.com>
2015-05-03 13:29:37 +0200

    Add boilerplate for 2.5_any.c

---
+++ 2.5_any.c
@@ -0,0 +1,14 @@
+/* 2.5_any.c - Write the function any(s1, s2) that takes two strings and
+ * returns the first location where a string from s2 occurs in s1. */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3) {
+        printf("Supply at least 2 parameters.\n");
+        EXIT_FAILURE;
+    }
+}
