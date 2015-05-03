33b0a07892c0bd04eb0ab8492c33c4e586ada470
Stefan Eng <eng_steff@hotmail.com>
2015-05-03 15:42:55 +0200

    Extract the n rightmost bits from y.

--- 2.6_setbits.c
+++ 2.6_setbits.c
@@ -1,9 +1,15 @@
 /* 2.6 setbits(x,p,n,y) - return x with the n bits
- * that begin at position set to the rightmost n
+ * that begin at position p set to the rightmost n
  * bits of y, other bits unchanged. */
 
 #include <stdlib.h>
 #include <stdio.h>
+
+int setbits(int x,int p,int n,int y)
+{
+    int ybits = ~(~0 << n) & y;
+    return ybits;
+}
 
 int main(int argc, char* argv[])
 {
