215e5bc399f0ec24b41bfe841646278819d1f274
Stefan Eng <eng_steff@hotmail.com>
2015-05-08 15:15:54 +0200

    2.8 created and running.

---
+++ 2.8_rightrot.c
@@ -0,0 +1,10 @@
+/* 2.8_rigthrot: write a function rightrot(x,n) that returns the value
+ * of the integer x rotated to the right by n bit positions. */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello world.\n");
+}
