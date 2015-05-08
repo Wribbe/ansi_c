7d72c42b49edf60bc10909f2a190a07a6fa7590d
Stefan Eng <eng_steff@hotmail.com>
2015-05-08 13:11:20 +0200

    Convert int to bits.

---
+++ 2.7_invert.c
@@ -0,0 +1,22 @@
+/* 2.7 write a function invert(x,p,n) that returns x with
+ * the n bits that begin at position p inverted. */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int get_bits(int num) {
+    if (num == 1 || num == 0 ) {
+        return num;
+    }
+    return (num%2 + get_bits((num/2))*10);
+}
+
+int stoi(char* string)
+{
+    return (int) strtol(string, NULL, 10);
+}
+
+int main (int argc, char* argv[]) {
+    printf("%d\n",get_bits(stoi(argv[1])));
+}
