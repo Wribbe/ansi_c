2330b3c6dc80bdf50838be7f88009ffea649f6b6
Stefan Eng <eng_steff@hotmail.com>
2015-05-09 11:09:39 +0200

    Check input and run book_bitcount.

--- 2.9_bitcount.c
+++ 2.9_bitcount.c
@@ -3,12 +3,44 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+int book_bitcount(unsigned x)
+{
+    int b;
+    for (b = 0; x != 0; x >>=1) {
+        if (x & 0x1) {
+            b++;
+        }
+    }
+    return b;
+}
+
+unsigned strtoi(char* string)
+{
+    return (unsigned) strtol(string, NULL, 10);
+}
 
 int main(int argc, char* argv[])
 {
-    int min_arguments = 1;
+    int min_arguments = 2;
     if (argc < min_arguments+1) {
-        printf("Need at least %d argument.\n", min_arguments);
+        printf("Need at least %d arguments.\n", min_arguments);
         exit(EXIT_FAILURE);
     }
+
+    int result, number = 0;
+    char* command = "";
+
+    number = strtoi(argv[2]);
+    command = argv[1];
+
+    if (strcmp(command,"book") == 0) {
+       result = book_bitcount(number);
+    } else {
+        printf("No such command %s.\n", command);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Result of running with %s: %d\n",command,result);
 }
