124c4b520e1b170974e6b9236a86e7144d43d392
Stefan Eng <eng_steff@hotmail.com>
2015-05-21 05:48:56 +0200

    Compile files in tests folder.

--- compile.sh
+++ compile.sh
@@ -7,6 +7,14 @@ if [ ! -d "$executable_dir" ]; then
   mkdir "$executable_dir"
 fi
 
+for file in ./tests/*; do
+  case "$file" in
+    *".c"*)
+      exec_name=$(echo $file | cut -c 9- | rev | cut -c 3- | rev)
+      cc "$file" -o "${executable_dir}/${exec_name}" $flags
+      ;;
+  esac
+done
 for file in ./*; do
   case "$file" in
     *".c"*)
