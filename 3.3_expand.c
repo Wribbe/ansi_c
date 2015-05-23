ca1526fa6271dc15e51574a239c009b5a637b56a
Stefan Eng <eng_steff@hotmail.com>
2015-05-23 16:02:37 +0200

    Set up some tests.

--- 3.3_expand.c
+++ 3.3_expand.c
@@ -17,7 +17,73 @@ int main(void)
 //-- Tests
 //------------------------------------------------------------------------------
 
+static char* expand_test_number_string()
+{
+    char* input = "1-9";
+    char* result;
+    char* expected = "123456789";
+    expand(input, &result);
+    mu_assert("expand did not produce the string 1 to 9.", strcmp(result, expected) == 0);
+    return 0;
+}
+
+static char* expand_test_alpha_string_lowercase()
+{
+    char* input = "a-z";
+    char* result;
+    char* expected = "abcdefghijklmnopqrstuvwxyz";
+    expand(input, &result);
+    mu_assert("expand did not produce the lower case alphabet.", strcmp(result, expected) == 0);
+    return 0;
+}
+
+static char* expand_test_alpha_string_uppercase()
+{
+    char* input = "A-Z";
+    char* result;
+    char* expected = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+    expand(input, &result);
+    mu_assert("expand did not produce the upper case alphabet.", strcmp(result, expected) == 0);
+    return 0;
+}
+
+static char* expand_test_alpha_string_mixed_case_lower_first()
+{
+    char* input = "a-Z";
+    char* result;
+    char* expected = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ";
+    expand(input, &result);
+    mu_assert("expand did not produce the mixed case alphabet (lower fist).", strcmp(result, expected) == 0);
+    return 0;
+}
+
+static char* expand_test_alpha_string_mixed_case_upper_first()
+{
+    char* input = "A-z";
+    char* result;
+    char* expected = "ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz";
+    expand(input, &result);
+    mu_assert("expand did not produce the mixed case alphabet (upper first).", strcmp(result, expected) == 0);
+    return 0;
+}
+
+static char* expand_test_alphanumeraical_lowercase()
+{
+    char* input = "a-z1-9";
+    char* result;
+    char* expected = "abcdefghijklmnopqrstuvwxyz123456789";
+    expand(input, &result);
+    mu_assert("expand dit not produce the correct lowercase alphanumerical string.", strcmp(result, expected) == 0);
+    return 0;
+}
+
 static char* all_tests()
 {
+    mu_run_test(expand_test_number_string);
+    mu_run_test(expand_test_alpha_string_lowercase);
+    mu_run_test(expand_test_alpha_string_uppercase);
+    mu_run_test(expand_test_alpha_string_mixed_case_lower_first);
+    mu_run_test(expand_test_alpha_string_mixed_case_upper_first);
+    mu_run_test(expand_test_alphanumeraical_lowercase);
     return 0;
 }
