fa8e77911fd14363b0f47fbcd7e277daf5eaf2de
Stefan Eng <eng_steff@hotmail.com>
2015-05-23 15:02:28 +0200

    Set up stub main and test for 3.2

---
+++ 3.3_expand.c
@@ -0,0 +1,23 @@
+/* 3.3_expand: write a function expand(s1,s2) that expands shorthand notations
+ * like a-z in the string s1 to the complete sequence abc..z in s2.
+ * Allow letters/numbers and upper and lower case.
+ * examples: a-b-c, a-z0-9, -a-z */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "minunit.h"
+
+int main(void)
+{
+    run_tests();
+}
+
+//------------------------------------------------------------------------------
+//-- Tests
+//------------------------------------------------------------------------------
+
+static char* all_tests()
+{
+    return 0;
+}
