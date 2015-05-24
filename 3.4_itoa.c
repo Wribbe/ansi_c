506620b0513fbc863c6f43544813f69a5a287901
Stefan Eng <eng_steff@hotmail.com>
2015-05-24 18:15:21 +0200

    Add main and test stub to 3.4_itoa.c

---
+++ 3.4_itoa.c
@@ -0,0 +1,24 @@
+/* Exercise 3.4_itoa.c: write a function that converts digits in a number
+ * to the corresponding characters in a string. */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "minunit.h"
+
+#define DEBUG 1
+
+int main(void)
+{
+    run_tests();
+}
+
+//------------------------------------------------------------------------------
+//-- Tests
+//------------------------------------------------------------------------------
+
+static char* all_tests()
+{
+    return 0;
+}
