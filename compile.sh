4bb743271d5f1264d73cc4998b41aaaa3ed86a7e
Stefan Eng <eng_steff@hotmail.com>
2015-05-24 16:04:36 +0200

    Build executables from lab folder and add --clean to script.

--- compile.sh
+++ compile.sh
@@ -7,10 +7,24 @@ if [ ! -d "$executable_dir" ]; then
   mkdir "$executable_dir"
 fi
 
-for file in ./tests/*; do
+if [ ! -z "$1" ]; then
+  case "$1" in
+    "--clean")
+      echo "Cleaning out executables and recompiling.."
+      rm -rf "$executable_dir"
+      mkdir "$executable_dir"
+    ;;
+    *)
+      echo `printf "No such command: %s" $1`
+      exit 1
+    ;;
+  esac
+fi
+
+for file in ./lab/*; do
   case "$file" in
     *".c"*)
-      exec_name=$(echo $file | cut -c 9- | rev | cut -c 3- | rev)
+      exec_name=$(echo $file | cut -c 7- | rev | cut -c 3- | rev)
       cc "$file" -o "${executable_dir}/${exec_name}" $flags
       ;;
   esac
