2c0750cd99bc96757815bb7757a1de0c1de79c58
Stefan Eng <eng_steff@hotmail.com>
2015-05-24 16:38:35 +0200

    Add debug printout macros.

--- lab/error_handling_debug_print.c
+++ lab/error_handling_debug_print.c
@@ -2,13 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 
+#define DEBUG 1
+
+#define debug_printf(fmt, ...) \
+                     do { if (DEBUG) fprintf(stderr, "%s:%d:%s(): " fmt, __FILE__, \
+                                             __LINE__, __func__, __VA_ARGS__); } while (0)
+
 typedef enum statuses {ERROR,SUCCESS} STATUS;
 
 STATUS check_input(char* input)
 {
     if (strcmp(input, "success") == 0) {
+        debug_printf("%s\n", "Trying input against \"sucess\".\n");
         return SUCCESS;
     } else {
+        debug_printf("%s\n", "Input check failed, returning ERROR.");
         return ERROR;
     }
 }
