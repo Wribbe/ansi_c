30232d9f1f1f5055a18d89baa54c1d0597cd218c
Stefan Eng <eng_steff@hotmail.com>
2015-05-24 09:37:39 +0200

    Convoluted paring somewhat working?

--- 3.3_expand.c
+++ 3.3_expand.c
@@ -5,8 +5,72 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "minunit.h"
+
+char* get_chars(char start, char end)
+{
+    int i, length, k;
+    length = (end-start)+2;
+    char* return_string;
+    return_string = malloc(sizeof(char)*length);
+    k = 0;
+    for(i = start; i<= end; i++) {
+        return_string[k++] = i;
+    }
+    return_string[k] = '\0';
+
+    return return_string;
+}
+
+int parse_input(char input[], char** tokens[])
+{
+    int length, num_dashes;
+
+    length = strlen(input);
+    char temp_string[length];
+    strcpy(temp_string, input);
+
+    num_dashes = 0;
+    for(int i = 0; i<length; i++) {
+        if (input[i] == '-') {
+            num_dashes++;
+        }
+    }
+    num_dashes++;
+
+    char** return_data = malloc(sizeof(char*)*(num_dashes));
+    int token_length, i;
+
+    i = 0;
+
+    char* current_token;
+    current_token = strtok(temp_string, "-");
+    while (current_token != NULL) {
+        token_length = strlen(current_token)+1;
+        char* return_string = malloc(sizeof(char)*token_length);
+        strcpy(return_string, current_token);
+        return_data[i++] = return_string;
+        current_token = strtok(NULL, "-");
+    }
+
+    *tokens = return_data;
+    return num_dashes;
+}
+
+void expand(char input[], char** result)
+{
+    int num_tokens;
+    char** tokens;
+    num_tokens = parse_input(input, &tokens);
+    *result = get_chars('1','9');
+    printf("num_tokens: %d\n",num_tokens);
+    int i;
+    for (i = 0; i<num_tokens; i++) {
+        printf("token#%d: %s\n",i,tokens[i]);
+    }
+}
 
 int main(void)
 {
