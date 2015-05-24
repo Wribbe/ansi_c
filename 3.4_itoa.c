774a5c4fed232fb73dcc89f650c2fd1c2554a03a
Stefan Eng <eng_steff@hotmail.com>
2015-05-24 19:55:29 +0200

    Add reverse and tests.

--- 3.4_itoa.c
+++ 3.4_itoa.c
@@ -9,6 +9,17 @@
 
 #define DEBUG 1
 
+STATUS reverse(char input[])
+{
+    int temp, i, j;
+
+    for(i = 0; j = strlen(input)-1, i < j; i++, j--) {
+        temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+    return SUCCESS;
+
 int main(void)
 {
     run_tests();
@@ -18,7 +29,17 @@ int main(void)
 //-- Tests
 //------------------------------------------------------------------------------
 
+static char* test_reverse()
+{
+    char input[] = "abc";
+    char* expected = "cab";
+    reverse(input);
+    mu_assert("Result from reveres incorrect.", strcmp(input, expected) == 0);
+    return 0;
+}
+
 static char* all_tests()
 {
+    mu_run_test(test_reverse);
     return 0;
 }
