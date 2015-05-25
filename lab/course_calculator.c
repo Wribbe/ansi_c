fefaacf13b8b47fa29033f49378add34575dff79
Stefan Eng <eng_steff@hotmail.com>
2015-05-25 19:42:18 +0200

    Detect missing semicolon, don't parse zero-len lines.

--- lab/course_calculator.c
+++ lab/course_calculator.c
@@ -31,10 +31,15 @@ float get_number(char string[])
             continue;
         }
     }
+    number[k] = '\0';
     if (strlen(number) == 0) {
+        for (int i = 0; i < strlen(string); i++) {
+            if (!isspace(string[i]) && !semicolon) {
+                exit(EXIT_FAILURE);
+            }
+        }
         return 0;
     } else {
-        number[k] = '\0';
         return strtof(number, NULL);
     }
 }
@@ -55,7 +60,8 @@ int main()
     while ((ch = getchar()) != EOF) {
         if (ch == '\n' || ch == '#') {
             current_line[i+1] = '\0';
-            if (current_line[0] != '#') {
+            if (current_line[0] != '#' && strlen(current_line) > 1) {
+                printf("checking value for line: %s\n",current_line);
                 total += get_number(current_line);
                 reset(current_line, 100);
             }
