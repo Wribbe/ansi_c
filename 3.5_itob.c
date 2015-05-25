3830a8737c7cc38d0a4fd852bb40831e91ab779e
Stefan Eng <eng_steff@hotmail.com>
2015-05-25 10:48:45 +0200

    Use code from 3.4 itoa.

---
+++ 3.5_itob.c
@@ -0,0 +1,92 @@
+/* Exercise 3.5_itob.c: Write a function itob(n,s,b) that converts the ingeger
+ * into a base b and outputs it as a string in s. */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "minunit.h"
+
+#define DEBUG 1
+
+typedef enum status {ERROR, SUCCESS} STATUS;
+
+STATUS reverse(char input[])
+{
+    int temp, i, j;
+
+    for(i = 0, j = strlen(input)-1; i < j; i++, j--) {
+        temp = input[i];
+        input[i] = input[j];
+        input[j] = temp;
+    }
+    return SUCCESS;
+}
+
+STATUS itoa(int n, char string[])
+{
+    int sign, ch, i;
+    sign = n;
+    i = 0;
+
+    do {
+        ch = abs(n % 10) + '0';
+        n = n/10;
+        string[i++] = ch;
+    } while (n != 0);
+
+    if (sign < 0) {
+        string[i++] = '-';
+    }
+
+    string[i] = '\0'; // The reversal only goes to i-1.
+    reverse(string);
+
+    return SUCCESS;
+}
+
+int main(void)
+{
+    run_tests();
+}
+
+//------------------------------------------------------------------------------
+//-- Tests
+//------------------------------------------------------------------------------
+
+static char* test_reverse()
+{
+    char input[] = "1234";
+    char* expected = "4321";
+    reverse(input);
+    mu_assert("Result from reveres incorrect.", strcmp(input, expected) == 0);
+    return 0;
+}
+
+static char* test_itoa()
+{
+    int pos_input = 1337;
+    int neg_input = -1337;
+
+    char* pos_expected = "1337";
+    char* neg_expected = "-1337";
+
+    char pos_result[5];
+    char neg_result[6];
+
+    itoa(pos_input, pos_result);
+    itoa(neg_input, neg_result);
+
+    printf("pos: %s, neg: %s\n",pos_result, neg_result);
+    mu_assert("failed to convert positive number.", strcmp(pos_result, pos_expected) == 0);
+    mu_assert("failed to convert negative number.", strcmp(neg_result, neg_expected) == 0);
+    return 0;
+}
+
+static char* all_tests()
+{
+    mu_run_test(test_reverse);
+    mu_run_test(test_itoa);
+    return 0;
+}
+
